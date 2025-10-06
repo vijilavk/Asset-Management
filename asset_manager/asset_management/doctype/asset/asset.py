5d25ea731a5c470027b1f293883d66876dcb34a7
vijila <vijilavk46@gmail.com>
2025-10-06 17:11:11 +0530

    feat:change assigned to field  clear based on the status

--- asset_manager/asset_management/doctype/asset/asset.py
+++ asset_manager/asset_management/doctype/asset/asset.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2025, vijila and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Asset(Document):
-	pass
+	def validate(self):
+		self.clear_value()
+
+	def clear_value(self):
+		if self.status=="Retired":
+			self.assigned_to=""
