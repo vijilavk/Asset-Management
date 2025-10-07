f95717690b71484c9eb4cdb36e821a1ff6f17de8
vijila <vijilavk46@gmail.com>
2025-10-07 10:48:17 +0530

    feat:maintenance log added to child table and asset status change to available and under processing based on maintenance log status

--- asset_manager/asset_management/doctype/maintenance_log/maintenance_log.py
+++ asset_manager/asset_management/doctype/maintenance_log/maintenance_log.py
@@ -1,9 +1,31 @@
 # Copyright (c) 2025, vijila and contributors
 # For license information, please see license.txt
 
-# import frappe
+
+import frappe
 from frappe.model.document import Document
 
-
 class MaintenanceLog(Document):
-	pass
+
+    def on_submit(self):
+        asset = frappe.get_doc("Asset", self.asset)
+        if asset:
+            asset.append('maintenance_logs', {
+                "maintenance_log_id": self.name,
+                "date": self.date,
+                "cost": self.cost,
+                "description": self.description
+            })
+            asset.save()
+            frappe.msgprint("Asset maintenance details added to maintenance log details table")
+
+    def validate(self):
+        if self.asset:
+            asset_exists = frappe.db.exists("Asset", self.asset)
+            if asset_exists:
+                asset = frappe.get_doc("Asset", self.asset)
+                if self.docstatus == 0:
+                    asset.status = "Under Maintenance"
+                elif self.docstatus == 1:
+                    asset.status = "Available"
+                asset.save()
